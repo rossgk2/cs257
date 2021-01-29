a5ed99cd47a10dfe873b35a5c2a8cd3411bbcde3
rossgk2 <groganr@carleton.edu>
2021-01-29 13:04:29 -0500

    Set up CREATE statements so that athletes will be the linking table

--- database/database-schema.sql
+++ database/database-schema.sql
@@ -5,25 +5,30 @@ CREATE TABLE athletes (
     Age float,
     Height float,
     Weight float,
-    Team text
+    Team_id integer,
+    Game_id integer,
+    City_id integer,
+    Sport_id integer,
+    Event_id integer,
+    Medal text
 );
 
 CREATE TABLE teams (
     id SERIAL,
     TeamName text,
-    NOC text
-);
-
-CREATE TABLE games (
-    id SERIAL,
-    Year integer,
-    Season text
+    NOC_id integer
 );
 
 CREATE TABLE NOCs (
     id SERIAL,
     Region text,
     Notes text
+);
+
+CREATE TABLE games (
+    id SERIAL,
+    Year integer,
+    Season text
 );
 
 CREATE TABLE cities (
