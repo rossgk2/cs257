d2a40cb67060399157d31171f4a101e75256d1dc
rossgk2 <groganr@carleton.edu>
2021-01-29 19:53:23 -0500

    Set up athletes_frame

--- database/convert.py
+++ database/convert.py
@@ -5,37 +5,6 @@ Written by Ross Grogan-Kaylor and Sam Gloss.
 import numpy as np
 import pandas as pd
 
-'''
-====================================================
-OUTLINE OF DATABASE DESIGN
-====================================================
-
-FIELDS WITH TABLES
-- athletes: SERIAL, name, sex, age, height, weight, team
-- teams: SERIAL, team_name, NOC
-- games: SERIAL, year, season 
-- NOCs: SERIAL, region, notes (use noc_regions.csv)
-- cities: SERIAL, city
-- sports: SERIAL, sport
-- events: SERIAL, event
-
-FIELDS WITHOUT TABLES
-- height, weight, sex, age, year, medal
-'''
-
-'''
-After this program runs, must use syntax such as
-
-\COPY athletes (Name, Sex, Age, Height, Weight, Team) FROM 'athletes.csv' DELIMITER ',' CSV NULL AS 'NULL';
-
-to load .csv files into psql. Simply using 
-
-\COPY athletes FROM 'athletes.csv' DELIMITER ',' CSV NULL AS 'NULL';
-
-does not work, because the psql attempt to read in a string value for the SERIAL id field.
-
-'''
-
 athlete_events = pd.read_csv(open("athlete_events.csv", "r"))
 noc_regions = pd.read_csv(open("noc_regions.csv", "r"))
 
@@ -44,12 +13,17 @@ athlete_events = athlete_events.replace(",", "", regex = True) # Get rid of comm
 athlete_events = athlete_events.fillna("NULL") # Replace empty cells with "NULL". (https://code.likeagirl.io/how-to-use-python-to-remove-or-modify-empty-values-in-a-csv-dataset-34426c816347)
 
 def main():
-    teams_frame = get_teams_frame_with_repeats()
+    teams_frame = get_teams_frame()
 
     cols = ["ID", "Name", "Sex", "Age", "Height", "Weight", "Games", "City", "Sport", "Event", "Medal"]
     athletes_cols_list = [athlete_events[c] for c in cols]
     athletes_frame = pd.concat(athletes_cols_list, axis = 1)
-    athletes_frame.insert(loc = cols.index("Games"), column = "team_id", value = athletes_frame.index)
+
+    # Insert the Team_ID column right before the Games column.
+    # possibly: teams_frame.drop_duplicates(subset = "Team")
+    ae_to_t_rows = df1_rows_to_df2(athlete_events, teams_frame, "Team")
+    print(ae_to_t_rows)
+    athletes_frame.insert(loc = cols.index("Games"), column = "Team_ID", value = ae_to_t_rows)
 
     # Write each pandas dataframe to a .csv file.
     dataframes = [athletes_frame, teams_frame]
@@ -59,22 +33,35 @@ def main():
         if df is not None:
             df.to_csv(fp + ".csv", header = False)
 
-def get_teams_frame_with_repeats():
+def get_teams_frame():
     # ae_to_noc_rows[i] will be the row in noc_regions that corresponds (via NOC names) to the ith row in athlete_events.
-    #
-    # We can imagine ae_to_noc_rows being constructed as follows: for each NOC name in athlete_events, find the row in
-    # noc_regions that contains that NOC name and add it to ae_to_noc_rows. After sort ae_to_noc_rows is filled,
-    # do NOT sort it.
-    #
-    # See # https://stackoverflow.com/questions/56658723/how-to-maintain-order-when-selecting-rows-in-pandas-dataframe.
-    idx = pd.Index(noc_regions["NOC"])
-    NOCindexer = idx.get_indexer(athlete_events["NOC"])
-    ae_to_noc_rows = noc_regions.index[NOCindexer]
+    ae_to_noc_rows = df1_rows_to_df2(athlete_events, noc_regions, "NOC")
 
+    # Now use ae_to_noc_rows to grab the rows from noc_regions that we want.
     NOCs = noc_regions.iloc[ae_to_noc_rows, :]
     NOCs = NOCs.reset_index()  # See https://stackoverflow.com/a/45056184.
 
-    # When we use ["Team"] inside the indexing operation instead of "Team", the result is a DataFrame instead of a Series.
-    return athlete_events[["Team"]].join(NOCs, how = "outer")
+    # Join the team names from athlete_events with the team NOC information. (Note: when we use ["Team"] inside the
+    # indexing operation instead of "Team", the result is a DataFrame instead of a Series).
+    teams_frame = athlete_events[["Team"]].join(NOCs, how = "outer")
+
+    # Remove rows that have the same team name. Note, the id's/indices of each row do not get reset, which is what we want.
+    # (No index resetting means that the id of the ith row in teams_frame is still equal to the id of the corresponding row
+    # in athlete_events).
+    teams_frame.drop_duplicates(subset = "Team")
+    return teams_frame
+
+# df1_rows_to_df2(df1, df2, col_name)[i] is the row in df2 that corresponds (via col_name) to the ith row in df1.
+def df1_rows_to_df2(df1, df2, col_name):
+    idx = pd.Index(df2[col_name])
+    indexer = idx.get_indexer(df1[col_name])
+    return df2.index[indexer]
+
+    # We can imagine that the returned value from this function is constructed as follows:
+    # - iterate down the column with column name "col_name" in df1
+    # - for each element x of this column, find the index of the row in df2 that contains x, and add this index to a list
+    # - return the list
+    #
+    # See # https://stackoverflow.com/questions/56658723/how-to-maintain-order-when-selecting-rows-in-pandas-dataframe.
 
 main()
