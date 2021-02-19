66e9d3ab7c53acd42b42b90a53a97f8b39f73d10
rossgk2 <groganr@carleton.edu>
2021-02-18 20:00:51 -0500

    Updated api-design.txt and added api-tests.py

---
+++ webapp/tests/api-tests.py
@@ -0,0 +1,69 @@
+'''
+   Ross Grogan-Kaylor and Jimmy Zhong
+'''
+import pokeapi
+import unittest
+
+class APICheckerTester(unittest.TestCase):
+    def setUp(self):
+    	self.pokeapi = pokeapi.pokeapi() 
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_types(self):
+    	'''
+   		Tests the API endpoint /types
+   		'''
+    	pass
+
+   	def test_abilities(self):
+   		'''
+   		Tests the API endpoint /abilities
+   		'''
+   		self.assertTrue(self.pokeapi.get_)
+
+   	def test_region(self):
+   		'''
+   		Tests the API endpoint /regions
+   		'''
+   		pass
+
+   	def test_growth_type(self):
+   		'''
+   		Tests the API endpoint /growth_types
+   		'''
+   		pass
+
+   	def test_egg_groups(self):
+   		'''
+   		Tests the API endpoint /egg_groups
+   		'''
+   		pass
+
+   	def test_pokemon(self):
+   		'''
+   		Tests the API endpoint pokemon/[_pokemon]
+   		'''
+   		pass
+
+   	def test_advanced_search(self):
+   		'''
+   		Tests the API endpoint /pokemon/?[id_range=_id_range]&[name_contains=_name_contains]&[legendary=_leg]&[type1=_type1]&[type2=_type2]&
+   		[ability1=_ability1]&[ability2=_ability2]&[hidden_ability=_hidden_ability]&[health_range=_health_range]&[attack_range=_attack_range]&
+   		[defense_range=_defense_range]&[special_attack_range=_special_attack_range]&[special_defense_range=_special_defense_range]&
+   		[speed_range=_speed_range]&[region=_region]&[catch_rate_range=_catch_rate_range]&[growth_type=&growth_type]&
+   		[male_percent_range=_male_percent_range]&[game=_game]&[egg_group1=_egg_group_1]&[egg_group2=_egg_group2]&
+   		[sort_field=_sort_field]&[sort_type=_sort_type]
+   		'''
+   		pass
+
+   	# self.assertRaises(ValueError, self.prime_checker.is_prime, -5)
+   	# self.assertTrue(self.prime_checker.is_prime(2))
+   	# self.assertFalse(self.prime_checker.is_prime(96))
+   	# self.assertEqual(self.prime_checker.get_primes_below(20), [2, 3, 5, 7, 11, 13, 17, 19])
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ webapp/tests/pokeapi.py
@@ -0,0 +1,2 @@
+class pokeapi():
+	pass
