c1f5c8157df84f0785b2a3c2bbae5b75b4c829de
rossgk2 <groganr@carleton.edu>
2021-03-03 20:23:13 -0500

    The pokemon_name portion of advanced search now works

--- webapp/static/advanced_search.js
+++ webapp/static/advanced_search.js
@@ -3,16 +3,50 @@
 */
 
 // The anonymous function inside $(document).ready() is called after the document has been loaded.
-$(document).ready(function() {
- 
-  // Initialize the select2 JQuery plugin
-  $(".search").select2(); //".search" is a CSS selector string
+$(document).ready(onReady)
 
-  // Read selected option
-  $('#search_button').click(function(){
-    var value = $("#pokemon_name option:selected").text();
+function onReady() {
+	// Initialize the select2 JQuery plugin
+	$(".search").select2(); //".search" is a CSS selector string
 
-    $('#result').html("selected value: " + value);
+	// Read selected option
+	$('#search_button').click(function() {
+		var value = $("#pokemon_name option:selected").text();
+		$('#result').html("selected value: " + value);
+	});
 
-  });
-});
+	loadPokemonNameDropdown();
+}
+
+function loadPokemonNameDropdown() {
+    var url = getAPIBaseURL() + "/query/ASC"
+    fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then(function(pokemonList) {
+    	// Construct the innerHTML for the dropdown
+    	var innerHTML = "<option> Click to search or select an option </option>\n";
+    	for (var i = 0; i < pokemonList.length; i ++) {
+			innerHTML += "<option> " + makePresentable(pokemonList[i]["pokemon_name"]) + " </option>\n";
+    	}
+    	innerHTML += "\n\n";
+
+    	// Assign this innerHTML
+    	var pokemonNameSearch = document.getElementById("pokemon_name_dropdown");
+		pokemonNameSearch.innerHTML = innerHTML;    	
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+// Helper functions
+
+function getAPIBaseURL() {
+    var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/api';
+    return baseURL;
+}
+
+function makePresentable(str) {
+   result = str.charAt(0).toUpperCase() + str.slice(1);
+   return result.replace("_", " ");
+}
