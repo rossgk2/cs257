7b88cdebd836c7b2f8adcc109045f129b6f8fa84
rossgk2 <groganr@carleton.edu>
2021-03-03 19:17:17 -0500

    Added a searchable dropdown menu to the advanced search

--- webapp/static/advanced_search.js
+++ webapp/static/advanced_search.js
@@ -1,5 +1,18 @@
-window.onload = initialize;
+/* The symbol $ is an alias for JQuery(). To be totally clear, this means that $ is used as the name of a function! In the below, $(document)
+ is the function $ acting on the argument whose name is document.
+*/
 
-function initialize() {
+// The anonymous function inside $(document).ready() is called after the document has been loaded.
+$(document).ready(function() {
+ 
+  // Initialize the select2 JQuery plugin
+  $(".search").select2(); //".search" is a CSS selector string
 
-}
+  // Read selected option
+  $('#search_button').click(function(){
+    var value = $("#pokemon_name option:selected").text();
+
+    $('#result').html("selected value: " + value);
+
+  });
+});
