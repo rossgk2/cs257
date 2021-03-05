e8c5ff50ca4df6e5a4f867b2ae0836cf602ddbbf
Juntao Zhong (Jimmy) <JuntaoZhong@users.noreply.github.com>
2021-03-04 19:32:07 -0600

    infinite scroll is working, and the pictures of pokemons are all clickable links now

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,3 @@
-user = 'ross'
-database = 'pokemon_db'
+user = 'postgres'
+database = 'grading'
 password = ''
--- webapp/static/webapp.js
+++ webapp/static/webapp.js
@@ -4,23 +4,123 @@
     Final Project 2021 Feburary 
 */
 $(document).ready(onReady)
+var numPokemonEachQuery = 30;
+var curNumPokemonOnThePage = 0;
+var numPokemonPerRow = 6;
+var reachTheEnd = 0; //0 is no, 1 is yes
+
 function onReady() {
     // Initialize the select2 JQuery plugin
     $(".search").select2(); //".search" is a CSS selector string
 
-    load_info("types", "type1_list_selection", "Select type 1");
-    load_info("types", "type2_list_selection", "Select type 2");
-    load_info("abilities", "ability_list_selection", "Select ability");
-    load_pokemon_cards()
+    load_info("types", "type1_list_selection", "all type1");
+    load_info("types", "type2_list_selection", "all type2");
+    load_info("abilities", "ability_list_selection", "all ability");
+    document.getElementById("pokemon_landing_display").innerHTML = load_wating_pic();
+    load_pokemon_cards("all", "all", "all", 0);
 
     // Read selected option
     $('#search_button').click(function() {
         var type1Selected = $("#type1_list_selection").val();
         var type2Selected = $("#type2_list_selection").val();
         var abilitySelected = $("#ability_list_selection").val(); //search in all ability1, 2, hidden
-        document.getElementById("result").innerHTML = "filter selection: " + type1Selected + " | " + type2Selected + " | " + abilitySelected;
+        document.getElementById("pokemon_landing_display").innerHTML = load_wating_pic();
+        curNumPokemonOnThePage = 0;
         load_pokemon_cards(type1Selected, type2Selected, abilitySelected)
     });
+
+    $(window).on("scroll", function() {
+        if (reachTheEnd == 0){
+            infinite_user_scroll();
+        }
+    })
+}
+
+
+function infinite_user_scroll(){
+    //from https://dev.to/sakun/a-super-simple-implementation-of-infinite-scrolling-3pnd
+    var scrollHeight = $(document).height();
+    var scrollPos = $(window).height() + $(window).scrollTop();
+    document.getElementById("result").innerHTML = "scroll height/scrollPos: " + scrollHeight + "/" + scrollPos;
+    if (scrollHeight - scrollPos <= 3) {
+        document.getElementById("pokemon_landing_display").innerHTML += load_wating_pic();
+        load_pokemon_cards("all", "all", "all");
+    }
+}
+
+function load_info(thisTypeOfInfo, htmlID, searchBarText) {
+    var url = getAPIBaseURL() + '/' + thisTypeOfInfo;
+    fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then(function(info) {
+        var listBody ='<option value = "all">' + searchBarText + '</option>\n';
+        for (var i=0; i < info.length; i++){
+            var rawElement = info[i];
+            listBody += '<option value = "' + rawElement + '">' + makePresentable(rawElement) + '</option>\n'
+        }
+        document.getElementById(htmlID).innerHTML = listBody
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+function load_pokemon_cards(type1Filter, type2Filter, abilityFilter){
+    var url = getAPIBaseURL() + "/advanced_search/DESC?order_by=pokedex_number&limit=" + numPokemonEachQuery + "&offset=" + curNumPokemonOnThePage;
+    if(type1Filter != "all") url += "&type1=" + type1Filter;
+    if(type2Filter != "all") url += "&type2=" + type2Filter;
+    if(abilityFilter != "all") url += "&composite_ability=" + abilityFilter;
+    //don't have pictures of pokemon with pokedex_number > 809; pokemon without images won't show up unless people search for them
+    if(type1Filter!="all" || type2Filter!="all" || abilityFilter!="all") url += "&pokedex_upper=3000";
+
+    return fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then(function(returnPokemon) {
+        var pokemonDisplayDiv = '<div class="container">\n<div class="row">';
+        var pokedexNum = 2000 //need this to each whether reach the end, for infinite scroll
+        for (var i = 0; i < returnPokemon.length; i++){
+            pokemonDisplayDiv += '<div class="col-2">'
+            var thisPokemon = returnPokemon[i];
+            pokedexNum = thisPokemon['pokedex_number'];
+
+            var rawName = thisPokemon['pokemon_name'];
+            var pokemonImagePath = getPokemonImagePath(rawName);
+            var pokemonImageHtml = '<img src="' + pokemonImagePath + '" alt="sorry, we do not have picture for this pokemon now" class="img-thumbnail">\n';
+            var pokeImageLink = '<a href="'+ getPokemonPageURL(rawName) + '">' + pokemonImageHtml + "</a>"
+
+            var firstLine = '<h6>(ID: ' + pokedexNum.toString() + ") " + makePresentable(rawName) + "</h>\n"
+
+            var typeImageLine1 = 'T1:<img src="../static/type_images/' + thisPokemon['type1'] + '.png" alt="something is wrong" class="img-thumbnail">\n';
+            var typeImageLine2 = 'T2:<img src="../static/type_images/' + thisPokemon['type2'] + '.png" alt="something is wrong" class="img-thumbnail">\n';
+            var secondLine = '<h6>\n' + typeImageLine1 + typeImageLine2 + '</h2>\n'; //type images
+            
+            pokemonDisplayDiv += pokeImageLink + firstLine + secondLine + '</div>'
+            if (i == numPokemonPerRow-1){ //change row every 6 cards
+                pokemonDisplayDiv += '</div>\n<div class="row">'
+            }
+        }
+        pokemonDisplayDiv += '</div>\n</div>';
+
+        curNumPokemonOnThePage += numPokemonEachQuery;
+        //reachTheEnd = checkReachTheEnd(url, pokedexNum);
+        //document.getElementById("result").innerHTML += "reach the end is: " + reachTheEnd;
+
+        page_content = document.getElementById("pokemon_landing_display");
+        newInnerHTML = page_content.innerHTML.replaceAll(load_wating_pic(), " ") + pokemonDisplayDiv;
+        page_content.innerHTML = newInnerHTML
+    })
+}
+
+function makePresentable(str) {
+    result = str.charAt(0).toUpperCase() + str.slice(1);
+    return result.replaceAll("_", " ");
+ }
+
+function checkReachTheEnd(url, pokedexNum){
+    //see whether we get any pokemon below this pokedexNum given the same filter criteria
+    var upper_bound_pokedexNum = pokedexNum -1;
+    checkURL = url + "&pokedex_upper=" + upper_bound_pokedexNum;
+    return fetch(checkURL)
 }
 
 function doesFileExist(urlToFile) {
@@ -49,64 +149,22 @@ function getAPIBaseURL() {
     return baseURL;
 }
 
-function load_info(thisTypeOfInfo, htmlID, searchBarText) {
-    var url = getAPIBaseURL() + '/' + thisTypeOfInfo;
-    fetch(url, {method: 'get'})
-    .then((response) => response.json())
-    .then(function(info) {
-        var listBody ='<option value = "all">' + searchBarText + '</option>\n';
-        for (var i=0; i < info.length; i++){
-            var rawElement = info[i];
-            listBody += '<option value = "' + rawElement + '">' + makePresentable(rawElement) + '</option>\n'
-        }
-        document.getElementById(htmlID).innerHTML = listBody
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+function getPokemonPageURL(pokemon_name){
+    var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/pokemon_page/' + pokemon_name;
+    return baseURL;
 }
 
-function load_pokemon_cards(type1Filter = "all", type2Filter = "all", abilityFilter = "all"){
-    //?pokemon_name=' + pokemon_dynamic_name
-    var num_pokemon_shown = 18;
-    var num_pokemon_per_row = 6;
-    var url = getAPIBaseURL() + '/advanced_search/DESC?limit=' + num_pokemon_shown + "&order_by=pokedex_number";
-    if(type1Filter != "all") url += "&type1=" + type1Filter;
-    if(type2Filter != "all") url += "&type2=" + type2Filter;
-    if(abilityFilter != "all") url += "&composite_ability=" + abilityFilter
-    //don't have pictures of pokemon with pokedex_number > 809; pokemon without images won't show up unless people search for them
-    if(type1Filter!="all" || type2Filter!="all" || abilityFilter!="all") url += "&pokedex_upper=3000" 
-
-    fetch(url, {method: 'get'})
-    .then((response) => response.json())
-    .then(function(returnPokemon) {
-        var pokemonDisplayDiv = '<div class="container">\n<div class="row">' //change row every 6 cards
-        for (var i = 0; i < returnPokemon.length; i++){
-            pokemonDisplayDiv += '<div class="col-2">'
-            var thisPokemon = returnPokemon[i];
-
-            var name = makePresentable(thisPokemon['pokemon_name']);
-            var pokemonImagePath = getPokemonImagePath(name);
-            var pokemonImageHtml = '<img src="' + pokemonImagePath + '" alt="sorry, we do not have picture for this pokemon now" class="img-thumbnail">\n\n';
-
-            var pokedexNum = thisPokemon['pokedex_number'];
-            var firstLine = '<h6>(ID: ' + pokedexNum.toString() + ") " + name + "</h>\n"
-
-            var typeImageLine1 = 'T1:<img src="../static/type_images/' + thisPokemon['type1'] + '.png" alt="something is wrong" class="img-thumbnail">\n'
-            var typeImageLine2 = 'T2:<img src="../static/type_images/' + thisPokemon['type2'] + '.png" alt="something is wrong" class="img-thumbnail">\n'
-            var secondLine = '<h6>\n' + typeImageLine1 + typeImageLine2 + '</h2>\n' //type images
-            
-            pokemonDisplayDiv += (pokemonImageHtml+ firstLine + secondLine + '</div>')
-            if (i == num_pokemon_per_row-1){
-                pokemonDisplayDiv += '</div>\n<div class="row">'
-            }
-        }
-        pokemonDisplayDiv += '</div>\n</div>'
-        document.getElementById("pokemon_landing_display").innerHTML = pokemonDisplayDiv
-    })
+function load_wating_pic(){
+    var single_loading_image = `\n<div class="col-2">
+    <img src="../static/pokemon_images/pokemon_ball_square.gif" class="img-thumbnail">
+    <h5>loading</h5>
+    <h6>T1:<img src="../static/type_images/null.png" class="img-thumbnail">
+    T2:<img src="../static/type_images/null.png" class="img-thumbnail"></h6>
+    </div>`;
+    var loading_display = '<div class="container">\n<div class="row">';
+    for (var i = 0; i < numPokemonPerRow; i++){
+        loading_display += single_loading_image
+    }
+    loading_display += '</div>\n</div>';
+    return loading_display;
 }
-
-function makePresentable(str) {
-    result = str.charAt(0).toUpperCase() + str.slice(1);
-    return result.replaceAll("_", " ");
- }
