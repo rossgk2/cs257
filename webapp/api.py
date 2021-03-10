fc08f5bb51cce7742ba7e46823dbb540e0489a49
rossgk2 <groganr@carleton.edu>
2021-03-10 17:01:06 -0500

    Used helper function to simplify api.py

--- webapp/api.py
+++ webapp/api.py
@@ -10,9 +10,9 @@ import config
 import psycopg2
 import calculate_effect
 
-api = flask.Blueprint('api', __name__)
+api = flask.Blueprint("api", __name__)
 
-def connect_database():
+def get_db_connection():
     ''' return a connection object to the postgres database, which is passed down for querying'''
     from config import database
     from config import user
@@ -25,111 +25,74 @@ def connect_database():
         print(e)
         exit()
 
-@api.route('/types')
-def get_types():
-    query = "SELECT * FROM types;"
-    db_connection = connect_database()
+def sql_query_helper(sql_table_name, row_accessor = None, remove_null = False, extra_query_text = ""):
+    '''
+    Inputs:
+    - sql_table_name is what it sounds like
+    - row_accessor must be a function that takes a "row" of a cursor object as input. The string that row_accessor obtains from each row
+    is appended to a list (we will call this list output_list) that is returned to the user.
+    - remove_null must either be a bool, or a function. If remove_null is a bool, then output_list is assumed to be a list of strings; all instances of "NULL"
+    are then removed from this list. If remove_null is a function, it is further assumed to be a function that acts on output_list and returns the result of
+    removing null entries from that list
+    - By default, the SQL query used in this function is "SELECT * FROM {sql_table_name};". If extra_query_text is not the empty string,
+    then the SQL query used will be "SELECT * FROM {sql_table_name} {extra_query_text};".
+
+    Output:
+    a JSON list of items that were obtained by using an SQL query
+
+
+    '''
+    query = "SELECT * FROM " + sql_table_name + " " + extra_query_text + ";"
+    db_connection = get_db_connection()
     cursor = db_connection.cursor()
     cursor.execute(query)
     output_list = []
     for row in cursor:
-        index, type_name = row
-        output_list.append(type_name)
-    output_list.remove("NULL")
-    return json.dumps(output_list)
-
-def helper(index):
-    query = "SELECT * FROM types;"
-    db_connection = connect_database()
-    cursor = db_connection.cursor()
-    cursor.execute(query)
-    output_list = []
-    for row in cursor:
-        output_list.append(row[index])
-    # optional: output_list.remove("NULL")
-    # optional: output_list.sort()
-    return json.dumps(output_list)
-
-@api.route('/abilities') 
-def get_abilities():
-    query = "SELECT * FROM abilities ORDER BY ability;"
-    cursor = connect_database().cursor()
-    cursor.execute(query)
-    output_list = []
-    for row in cursor:
-        index, ability, ability_description = row
-        output_list.append(ability)
-    output_list.remove("NULL")
-    return json.dumps(output_list)
-
-@api.route('/ability_description/<ability>') 
-def get_abilities_description(ability):
-    return_description = "no description"
-    if ability.lower() == "null":
-        return json.dumps(return_description)
+        output_list.append(row_accessor(row))
     
-    like_argument = "%" + ability + "%"
-    query = "SELECT * FROM abilities WHERE ability ILIKE %s;"
-    cursor = connect_database().cursor()
-    cursor.execute(query, (like_argument,))
-    # there should only be 1 return value
-    for row in cursor:
-        print(row)
-        return_description = row[2]
-    return json.dumps(return_description)
+    # If remove_null is boolean and remove_null is also True, then remove "NULL" from the output list.
+    # Otherwise, remove_null is a function that will act on output_list to return the output_list
+    # after "NULL" has been removed.
+    if type(remove_null) is bool:
+        if remove_null:
+            output_list.remove("NULL")
+    else:
+        output_list = remove_null(output_list)
+    if sql_table_name == "abilities":
+        print(output_list)
+    return json.dumps(output_list)
 
-@api.route('/legendaries')
+@api.route("/types")
+def get_types():
+    return sql_query_helper(sql_table_name = "types", row_accessor = lambda row : row[1], remove_null = True)
+
+@api.route("/abilities") 
+def get_abilities():
+    return sql_query_helper(sql_table_name = "abilities", 
+        row_accessor = lambda row : {"ability" : row[1], "ability_description" : row[2]},
+        remove_null = lambda output_list : [x for x in output_list if x["ability"] != "NULL"], 
+         extra_query_text = "ORDER BY ability")
+
+@api.route("/legendaries")
 def get_legendaries():
-    query = "SELECT * FROM legendaries;"
-    db_connection = connect_database()
-    cursor = db_connection.cursor()
-    cursor.execute(query)
-    output_list = []
-    for row in cursor:
-        index, type_name = row
-        output_list.append(type_name)
-    return json.dumps(output_list)
+    #Don't remove null; we want to use it and relabel it as "Not legendary"
+    return sql_query_helper(sql_table_name = "legendaries", row_accessor = lambda row : row[1], remove_null = False)
 
-@api.route('/regions')
+@api.route("/regions")
 def get_regions():
-    query = "SELECT * FROM regions;"
-    db_connection = connect_database()
-    cursor = db_connection.cursor()
-    cursor.execute(query)
-    output_list = []
-    for row in cursor:
-        index, region = row
-        output_list.append(region)
-    return json.dumps(output_list)
+    return sql_query_helper(sql_table_name = "regions", row_accessor = lambda row : row[1], remove_null = False)
 
-@api.route('/games')
+@api.route("/games")
 def get_games():
-    query = "SELECT * FROM games;"
-    db_connection = connect_database()
-    cursor = db_connection.cursor()
-    cursor.execute(query)
-    output_list = []
-    for row in cursor:
-        index, game = row
-        output_list.append(game)
-    return json.dumps(output_list)
+    return sql_query_helper(sql_table_name = "games", row_accessor = lambda row : row[1], remove_null = False)
 
-@api.route('/egg_groups')
+@api.route("/egg_groups")
 def get_egg_groups():
-    query = "SELECT * FROM egg_groups;"
-    db_connection = connect_database()
-    cursor = db_connection.cursor()
-    cursor.execute(query)
-    output_list = []
-    for row in cursor:
-        index, egg_group = row
-        output_list.append(egg_group)
-    output_list.remove("NULL")
-    return json.dumps(output_list)
+    return sql_query_helper(sql_table_name = "egg_groups", row_accessor = lambda row : row[1], remove_null = True)
 
 # use script calculate_effect.py, not the database
 # return all abnormal effects (non 1) into 2 list: supereffect (>1), and undereffect (<1)
-@api.route('/supereffect_cal/<my_type1>/<my_type2>')
+@api.route("/supereffect_cal/<my_type1>/<my_type2>")
 def get_supereffect(my_type1, my_type2):
     my_type1 = my_type1.lower()
     my_type2 = my_type2.lower()
@@ -144,7 +107,7 @@ def get_supereffect(my_type1, my_type2):
     result.sort(key = lambda i: i[1]) #sort by effect size
     return json.dumps(result)
 
-@api.route('advanced_search/<order>')
+@api.route("advanced_search/<order>")
 def advanced_search(order):
     ''' 
     Examples of how to use this endpoint:
@@ -256,7 +219,7 @@ def advanced_search(order):
     query = query + ";"
 
 
-    db_connection = connect_database()
+    db_connection = get_db_connection()
     cursor = db_connection.cursor()
     
     parameter_list = tuple(parameter_list)
