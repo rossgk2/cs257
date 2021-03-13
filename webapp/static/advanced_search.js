fa452cb722b02ae1371de35268fefa95de891019
rossgk2 <groganr@carleton.edu>
2021-03-13 14:11:16 -0500

    Changed layout of card for advanced search

--- webapp/static/advanced_search.js
+++ webapp/static/advanced_search.js
@@ -29,13 +29,19 @@ function loadInfinitePokemonCardScroller() {
     var scrollContainer = display;
     var searchButton = document.getElementById("search_button");
 
+    getSimplePokemonCard = function(pokemon) {
+    	var pokedexNum = pokemon['pokedex_number'];
+        var name = pokemon['pokemon_name'];
+        var pokemonImageHtml = getPokemonImageWithLink(name);
+        
+        var firstLine = `<h6>(ID: ${pokedexNum}) ${makePresentable(name)}</h6>\n`;
+        var secondLine = `<h6>${getTypeImageHTML(pokemon['type1'])} ${getTypeImageHTML(pokemon['type2'])}</h6>\n`;
+        return pokemonImageHtml + firstLine + secondLine;
+    }
+
     getPokemonCard = function(pokemon) {
         var rawName = pokemon["pokemon_name"];
-		var returnString = getPokemonImageWithLink(rawName);
-		returnString += '<div class="invisible-vertical-line"></div>';
-
-		var keyDisplayConversion = {"Name" : "pokemon_name", "ID" : "pokedex_number", "Type1" : "type1", "Type2" : "type2"};
-		returnString += dualColTableBuilder(["Name", "ID", "Type1", "Type2"], pokemon, keyDisplayConversion); //first tabl
+		var returnString = '<div class = "simple_card">' + getSimplePokemonCard(pokemon) + '</div>';
 		returnString += '<div class="invisible-vertical-line"></div>';
 		returnString += dualColTableBuilder(["attack", "special_attack", "defense", "special_defense", "health", "speed"], pokemon, null); //second table
 		return returnString;
