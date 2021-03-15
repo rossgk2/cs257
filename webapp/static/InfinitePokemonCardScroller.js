4a3867986d636ddca4c8e7274cca6ff4a2bf2719
rossgk2 <groganr@carleton.edu>
2021-03-15 16:50:05 -0400

    Added an explanatory comment to InfinitePokemonCardScroller.js

--- webapp/static/InfinitePokemonCardScroller.js
+++ webapp/static/InfinitePokemonCardScroller.js
@@ -1,3 +1,24 @@
+/* This class is used to set up a scrolling subwindow that displays "Pokemon cards" of the potentially many Pokemon that satisfy a search query.
+
+- display is the HTML element which the inner HTML of this scrolling subwindow should be written to
+- searchButton is the particular <input type = "button"> that will cause the infinite scrolling window to update
+when it is clicked.
+- scrollContainer is the HTML element whose onscroll() event handler is to be associated with the infinite scrolling window.
+If the infinite scrolling window is to take up the whole screen, use scrollContainer = window. Otherwise, it is probably
+some <div> in your HTML code.
+
+Exactly what constitutes a Pokemon card is up to the user; the user must provide to the constructor a function "getPokemonCard(pokemon)"" that acts
+ on "pokemon", which is a JSON dictionary, and returns the inner HTML that represents a Pokemon card.
+
+The user also decides which API URL is queried when the button is clicked or when the scrollContainer.onscroll() fires, since they
+ must provide to the constructor a function "getQueryURLOnUpdate()", accepting no arguments, that returns the URL prefixed with
+ "<this-websites-address>/api/" to be visited. When implementing getQueryURLOnUpdate(), the user will likely pull values from input fields 
+ to decide which URL to visit.
+
+Lastly (these parameters are not so important):
+- numPokemonEachQuery is the maximum number of Pokemon that any API query handled by a InfinitePokemonCardScroller instance will return
+- numPokemonPerRow is the number of Pokemon cards per row in the infinite scrolling window 
+*/
 function InfinitePokemonCardScroller(display, scrollContainer, searchButton, getQueryURLOnUpdate, getPokemonCard, numPokemonEachQuery, numPokemonPerRow) {
 	this.display = display;
 	this.scrollContainer = scrollContainer;
